62ec9d5578c1b7b96ec38cea562e9006c048f099
Rohit <rohit192ranjan@gmail.com>
2023-02-22 01:19:38 +0530

    Deploying v2.0

--- app.js
+++ app.js
@@ -6,13 +6,11 @@ var _ = require('lodash');
 require("dotenv").config();
 //const { request } = require("express");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "The daily blog project is an initiative aimed at encouraging individuals to cultivate a daily writing habit by sharing their thoughts, experiences, and insights through a blog. The project emphasizes the benefits of regular writing, including improved communication skills, enhanced creativity, and better self-expression. Through the daily blog project, participants can connect with a broader community of writers, receive feedback, and gain exposure to diverse perspectives and ideas. By committing to writing daily, individuals can hone their writing skills, overcome writer's block, and develop a greater sense of self-awareness. The daily blog project is a fantastic way to explore one's interests, share knowledge, and build an online presence.";
 
 const app = express();
 
-// mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+//mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
 mongoose.connect(process.env.Mongodb_link, {useNewUrlParser: true});
 
 const postSchema = {
@@ -31,6 +29,7 @@ app.get("/posts/:postId", function(req, res){
   const requestedPostId = req.params.postId;
   Post.findOne({_id: requestedPostId}, function(err, post){
     res.render("post", {
+      css: "styles.css",
       title: post.title,
       content: post.content
     });
@@ -41,6 +40,7 @@ app.get("/posts/:postId", function(req, res){
 app.get("/", function(req, res){
   Post.find({}, function(err, posts){
     res.render("home", {
+      css: "styles.css",
       homeStartingContent: homeStartingContent,
       posts: posts
       });
@@ -48,15 +48,15 @@ app.get("/", function(req, res){
 });
 
 app.get("/about", function(req, res){
-  res.render('about',{aboutContent:aboutContent});
+  res.render('about',{css : "styles.css"});
 });
 
 app.get("/contact", function(req, res){
-  res.render('contact',{contactContent:contactContent});
+  res.render('contact',{css : "contact.css"});
 });
 
 app.get("/compose", function(req, res){
-  res.render('compose');
+  res.render('compose',{css : "styles.css"});
 });
 
 app.post("/compose", function(req, res){
